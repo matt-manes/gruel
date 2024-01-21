77ac121704acb2b72a6ec0d8b3f287749931adeb
matt manes <mattmanes@pm.me>
2024-01-21 12:41:17 -0600

    refactor: BREAKING: separate gruel finding logic from `Brewer` class

--- src/gruel/brewer.py
+++ src/gruel/brewer.py
@@ -3,24 +3,109 @@ import importlib
 import importlib.machinery
 import importlib.util
 import inspect
-from typing import Any
+from types import ModuleType
+from typing import Any, Sequence
 
 import loggi
 import quickpool
 from pathier import Pathier, Pathish
-from younotyou import younotyou
+from younotyou import Matcher, younotyou
 
 from gruel import Gruel
+
+
+class GruelFinder:
+    def __init__(
+        self,
+        subgruel_classes: list[str] = ["*"],
+        file_exclude_patterns: list[str] = [],
+        scan_path: Pathier | None = None,
+        file_include_patterns: list[str] = ["*.py"],
+        recursive: bool = True,
+        log_dir: Pathish | None = None,
+    ):
+        self.subgruel_classes = subgruel_classes
+        self.file_exclude_patterns = file_exclude_patterns
+        self.scan_path = scan_path or Pathier.cwd()
+        self.file_include_patterns = file_include_patterns
+        self.recursive = recursive
+        self.logger = loggi.getLogger(
+            "gruel_finder", Pathier(log_dir) if log_dir else Pathier.cwd()
+        )
+
+    def get_bases(self, object: Any) -> list[Any]:
+        """Returns a recursive list of all the classes `object` inherits from."""
+        parents = []
+        bases = object.__bases__
+        if not bases:
+            return parents
+        for base in bases:
+            parents.append(base)
+            parents.extend(self.get_bases(base))
+        return parents
+
+    def is_subgruel(self, object: Any) -> bool:
+        """Returns whether `object` inherits from `Gruel` somewhere in its ancestory."""
+        if not inspect.isclass(object) or Gruel not in self.get_bases(object):
+            return False
+        return True
+
+    def glob_files(self) -> list[Pathier]:
+        """Search `self.scan_path` for files according to `self.file_include_patterns` and `self.file_exclude_patterns`.
+
+        Returns the file list."""
+        globber = self.scan_path.rglob if self.recursive else self.scan_path.glob
+        files = [
+            str(
+                file
+                for pattern in self.file_include_patterns
+                for file in globber(pattern)
+            )
+        ]
+        files = [
+            Pathier(file)
+            for file in younotyou(files, exclude_patterns=self.file_exclude_patterns)
+        ]
+        return files
+
+    def load_module_from_file(self, file: Pathier) -> ModuleType | None:
+        """Attempts to load and return a module from `file`."""
+        module_name = file.stem
+        try:
+            module = importlib.machinery.SourceFileLoader(
+                module_name, str(file)
+            ).load_module()
+            self.logger.info(f"Successfully imported `{module_name}` from `{file}`.")
+            return module
+        except Exception as e:
+            self.logger.exception(f"Failed to load `{module_name}` from `{file}`.")
+
+    def strain_for_gruel(self, modules: list[ModuleType]) -> list[Gruel]:
+        """Searches `modules` for classes that inherit from `Gruel` and are in `self.subgruel_classes`.
+
+        Returns the list of classes."""
+        matcher = Matcher(self.subgruel_classes)
+        return [
+            getattr(module, class_)
+            for module in modules
+            for class_ in dir(module)
+            if class_ in matcher and self.is_subgruel(getattr(module, class_))
+        ]
+
+    def find(self) -> list[Gruel]:
+        """Run the scan and return `Gruel` subclasses."""
+        files = self.glob_files()
+        modules = []
+        for file in files:
+            if module := self.load_module_from_file(file):
+                modules.append(module)
+        return self.strain_for_gruel(modules)
 
 
 class Brewer:
     def __init__(
         self,
-        subgruel_classes: list[str],
-        file_exclude_patterns: list[str] = [],
-        scan_path: Pathish = Pathier.cwd(),
-        file_include_patterns: list[str] = ["*.py"],
-        recursive: bool = True,
+        scrapers: Sequence[Any],
         log_dir: Pathish | None = None,
     ):
         """Run `Gruel` scrapers.
@@ -44,11 +129,7 @@ class Brewer:
         >>> brewer = Brewer(["VenueScraper"], ["*template*", "*giggruel*"], "scrapers")
         >>> brewer.brew()"""
         self._init_logger(log_dir)
-        self.subgruel_classes = subgruel_classes
-        self.file_exclude_patterns = file_exclude_patterns
-        self.file_include_patterns = file_include_patterns
-        self.scan_path = Pathier(scan_path)
-        self.recursive = recursive
+        self.scrapers = scrapers
 
     def _init_logger(self, log_dir: Pathish | None = None):
         # When Brewer is subclassed, use that file's stem instead of `brewer`
@@ -60,135 +141,45 @@ class Brewer:
             log_name = Pathier(__file__).stem
         self.logger = loggi.getLogger(log_name, log_dir)
 
-    def load_scrapers(self) -> list[Gruel]:
-        """Load scraper classes that inherit from `Gruel`.
-
-        NOTE: Classes are loaded, but scraper objects are not instantiated until the `scrape()` method is called.
-
-        #### :params:
-
-        `directory`: The path to scan for scraper classes.
-
-        `class_names`: A list of class names for scrapers that should be loaded.
-        In order to be loaded, a scraper class must have a name in this list and have `Gruel` somewhere in its inheritance hierarchy.
-
-        `include_patterns`: Files that match these patterns will be scanned.
-
-        `exclude_patterns`: Files that match these patterns will not be scanned.
-
-        `recursive`: Whether the search should be recursive or not.
-
-        >>> load_scrapers("getToTheGig/scrapers", ["VenueScraper"], ["*.py"], ["*template*", "*giggruel*"])
-        """
-        globber = self.scan_path.glob
-        if self.recursive:
-            globber = self.scan_path.rglob
-        files = [
-            str(file)
-            for pattern in self.file_include_patterns
-            for file in globber(pattern)
-        ]
-        files = younotyou(files, exclude_patterns=self.file_exclude_patterns)
-        self.modules = {}
-        self._module_names = []
-        for file in files:
-            module_name = Pathier(file).stem
-            try:
-                module = importlib.machinery.SourceFileLoader(
-                    module_name, file
-                ).load_module()
-            except Exception as e:
-                self.logger.exception(
-                    f"Failed to load module '{module_name}' from '{file}'."
-                )
-            else:
-                self._module_names.append(module_name)
-                self.modules[module] = module
-        gruels = [
-            getattr(module, class_)
-            for module in self.modules.values()
-            for class_ in self.subgruel_classes
-            if class_ in dir(module) and self.is_subgruel(getattr(module, class_))
-        ]
-        self.logger.info(
-            "\n".join(
-                [f"Imported {len(gruels)} scrapers: "]
-                + [str(gruel) for gruel in gruels]
-            )
-        )
-        return gruels
-
-    def pop_modules(self):
-        """Unload modules."""
-        for module in self.modules:
-            del module
-        self._module_names = []
-
-    def get_bases(self, object: Any) -> list[Any]:
-        """Returns a recursive list of all the classes `object` inherits from."""
-        parents = []
-        bases = object.__bases__
-        if not bases:
-            return parents
-        for base in bases:
-            parents.append(base)
-            parents.extend(self.get_bases(base))
-        return parents
-
-    def is_subgruel(self, object: Any) -> bool:
-        """Returns whether `object` inherits from `Gruel` somewhere in its ancestory."""
-        if not inspect.isclass(object) or Gruel not in self.get_bases(object):
-            return False
-        return True
-
     def prescrape_chores(self):
         """Override to add any tasks to be done before running the scrapers."""
         ...
 
     def postscrape_chores(self):
         """Override to add any tasks to be done after running the scrapers."""
-        self.pop_modules()
+        ...
 
-    def scrape(self, scrapers: list[Gruel]):
+    def scrape(self) -> list[Any]:
         """Run the `scrape()` method for each scraper in `scrapers`.
 
         Execution is multithreaded."""
         execute = lambda scraper: scraper().scrape()
         pool = quickpool.ThreadPool(
-            [execute] * len(scrapers), [(scraper,) for scraper in scrapers]
+            [execute] * len(self.scrapers), [(scraper,) for scraper in self.scrapers]
         )
-        pool.execute()
+        return pool.execute()
 
-    def logprint(self, message: str):
-        """Log and print `message`."""
-        self.logger.info(message)
-        print(message)
-
-    def brew(self):
+    def brew(self) -> list[Any] | None:
         """Execute pipeline.
 
         1. self.prescrape_chores()
-        2. self.load_scrapers()
-        3. self.scrape()
-        4. self.postscrape_chores()"""
+        2. self.scrape()
+        3. self.postscrape_chores()"""
 
         try:
-            self.logprint("Beginning brew")
+            self.logger.logprint("Beginning brew")
             # 1--------------------------------------------
-            self.logprint("Executing prescrape chores")
+            self.logger.logprint("Executing prescrape chores")
             self.prescrape_chores()
             # 2--------------------------------------------
-            self.logprint("Loading scrapers")
-            scrapers = self.load_scrapers()
-            print(f"Loaded {len(scrapers)} scrapers")
-            # 3--------------------------------------------
-            self.logprint("Starting scrape")
-            self.scrape(scrapers)
-            self.logprint("Scrape complete")
+            self.logger.logprint("Starting scrape")
+            results = self.scrape()
+            self.logger.logprint("Scrape complete")
             # 4--------------------------------------------
-            self.logprint("Executing postscrape chores")
+            self.logger.logprint("Executing postscrape chores")
             self.postscrape_chores()
-            self.logprint("Brew complete")
+            self.logger.logprint("Brew complete")
+            return results
         except Exception as e:
             print(e)
             self.logger.exception("Exception occured during brew():")
@@ -248,12 +239,16 @@ def get_args() -> argparse.Namespace:
 def main(args: argparse.Namespace | None = None):
     if not args:
         args = get_args()
-    brewer = Brewer(
+    finder = GruelFinder(
         args.subgruel_classes,
         args.excludes,
         args.path,
         args.includes,
         args.recursive,
+        args.log_dir,
+    )
+    brewer = Brewer(
+        finder.find(),
         args.log_dir,
     )
     brewer.brew()
