d5c223f8091be3eab40fad531a33adbf5393faee
matt manes <mattmanes@pm.me>
2024-02-17 21:07:55 -0600

    chore: build v3.0.1

--- src/gruel/__init__.py
+++ src/gruel/__init__.py
@@ -1,5 +1,5 @@
 from .brewer import Brewer, GruelFinder
 from .grueler import Gruel, ParsableItem, request
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __all__ = ["Brewer", "GruelFinder", "Gruel", "ParsableItem", "request"]
