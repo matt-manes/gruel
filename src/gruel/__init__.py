8f27484f81a9a18257fd9bbbba8e9fc2dc5bec9c
matt manes <mattmanes@pm.me>
2024-04-30 13:37:46 -0500

    refactor: rename file

--- src/gruel/__init__.py
+++ src/gruel/__init__.py
@@ -1,6 +1,6 @@
 from .brewer import Brewer, GruelFinder
 from .core import Gruel
-from .request import Session, request, Response
+from .requests import Session, request, Response
 
 __version__ = "3.0.1"
 __all__ = ["Brewer", "GruelFinder", "Gruel", "request", "Session", "Response"]
