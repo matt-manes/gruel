fbc6f8ae567d9dd0979fee402fe16b9cbb7ae64a
matt manes <mattmanes@pm.me>
2024-04-30 17:12:45 -0500

    refactor: export `retry_on_codes`

--- src/gruel/__init__.py
+++ src/gruel/__init__.py
@@ -1,6 +1,14 @@
 from .brewer import Brewer, GruelFinder
 from .core import Gruel
-from .requests import Session, request, Response
+from .requests import Session, request, Response, retry_on_codes
 
 __version__ = "3.0.1"
-__all__ = ["Brewer", "GruelFinder", "Gruel", "request", "Session", "Response"]
+__all__ = [
+    "Brewer",
+    "GruelFinder",
+    "Gruel",
+    "request",
+    "Session",
+    "Response",
+    "retry_on_codes",
+]
