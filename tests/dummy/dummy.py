7b109c2bed8a2df21af3369944e767d68c6b26b6
matt manes <mattmanes@pm.me>
2024-05-03 10:32:36 -0500

    tests: update test

--- tests/dummy/dummy.py
+++ tests/dummy/dummy.py
@@ -1,3 +1,4 @@
+from typing_extensions import Any
 from pathier import Pathier
 
 from gruel import Gruel
@@ -7,6 +8,18 @@ class DummyGruel(Gruel):
     def __init__(self, check_val: int, name: str):
         super().__init__(name)
         self.check_val = check_val
+
+    def get_source(self, *args: Any, **kwargs: Any) -> Any:
+        return super().get_source(*args, **kwargs)
+
+    def get_parsable_items(self, source: Any) -> list[Any]:
+        return []
+
+    def parse_item(self, item: Any) -> Any:
+        return None
+
+    def store_item(self, item: Any) -> Any:
+        return None
 
     def scrape(self) -> tuple[int, str]:
         self.logger.close()
