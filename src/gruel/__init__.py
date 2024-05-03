de58b2ec29a89df51ce520dddc914f084316b835
matt manes <mattmanes@pm.me>
2024-05-03 10:32:16 -0500

    chore: add to exported objects list

--- src/gruel/__init__.py
+++ src/gruel/__init__.py
@@ -1,14 +1,20 @@
 from .brewer import Brewer, GruelFinder
-from .core import Gruel
-from .requests import Session, request, Response, retry_on_codes
+from .core import ChoresMixin, Gruel, ParserMixin
+from .crawler import Crawler, CrawlScraper, UrlManager
+from .requests import Response, Session, request, retry_on_codes
 
 __version__ = "3.0.1"
 __all__ = [
     "Brewer",
     "GruelFinder",
     "Gruel",
+    "ChoresMixin",
+    "ParserMixin",
     "request",
     "Session",
     "Response",
     "retry_on_codes",
+    "UrlManager",
+    "Crawler",
+    "CrawlScraper",
 ]
