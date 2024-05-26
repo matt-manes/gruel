5779692080879662d9a08e883d27426f6f2cf20b
matt manes <mattmanes@pm.me>
2024-05-26 16:13:53 -0500

    chore: build v4.3.0

--- src/gruel/__init__.py
+++ src/gruel/__init__.py
@@ -10,7 +10,7 @@ from .crawler import (
 )
 from .requests import Response, Session, request, retry_on_codes
 
-__version__ = "4.2.2"
+__version__ = "4.3.0"
 __all__ = [
     "Brewer",
     "GruelFinder",
